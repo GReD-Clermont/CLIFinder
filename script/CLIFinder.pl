8363b4486ab4967b9688770c1b3a984376e8bd21
Pierre Pouchin <pierre.pouchin@uca.fr>
2020-02-07 00:18:41 +0100

    Compacted R script

--- script/CLIFinder.pl
+++ script/CLIFinder.pl
@@ -918,188 +918,113 @@ sub save_csv{
   
   # Create bridge between Perl and R
   my $R = Statistics::R->new();
-  eval{
-    $R->run(
-      qq`
-      rm(list=ls())
-      library(GenomicRanges)
+  eval
+  {
+    $R->run(qq`
       library(plyr)
+      library(GenomicRanges)
       chim<-read.delim("$out1")
-
       chim<-chim[order(chim[,$#fastq1+7],decreasing=F),]
       chim<-chim[order(chim[,2],decreasing=F),]
       chr<-sub("chr","",as.character(chim[,1]))
       chim<-chim[order(as.numeric(chr)),]
-      
-      grchim <- GRanges(seqnames = chim[,1],
-                    IRanges(start = chim[,2],
-                            end = chim[,3]),strand=chim[,4])
-      
-      
+      grchim <- GRanges(seqnames = chim[,1], IRanges(start = chim[,2], end = chim[,3]),strand=chim[,4])
       grchim\$ID<-paste("Id_",1:length(chim[,1]),sep="")
-      
       mcols(grchim)<-cbind(mcols(grchim),chim[,5:($#fastq1+9)])
-      
-      
       grfusR<- union(grchim,grchim)
-      
       position<-as.data.frame(findOverlaps(grfusR,grchim))
-      
       grfusR\$dup<- as.vector(table(position[,1]))
-      
       position2<-as.data.frame(findOverlaps(grfusR[grfusR\$dup>1],grchim))
-      
-      
       grfusR2<-grfusR
       strand(grfusR2)<- "+"
       gr3<-union(grfusR2,grfusR2)
       position3<- as.data.frame(findOverlaps(gr3,grfusR2))
       gr3\$dup<-as.vector(table(position3[,1]))
-      
       position3<-as.data.frame(findOverlaps(gr3[gr3\$dup>1],grfusR2))
       grfusR\$info<-"no"
       grfusR\$info [position3[,2]]<-"overlap sens opposé"
-      
       grfusR\$ID<-"Id"
       grfusR\$ID[position[!duplicated(position[,1]),1]]<-grchim\$ID[position[!duplicated(position[,1]),2]]
-      
       if(nrow(position2)!=0)
       {
         result <- aggregate(position2[,2] ~ position2[,1], data = position2, paste, collapse = "_")
         grfusR\$ID[grfusR\$dup>1]<-paste("ID",result[,2],sep="_")
       }
-      
       mcols(grfusR)<-cbind(mcols(grfusR), mcols(grchim[position[!duplicated(position[,1]),2]]))
-      
       min<-ddply(as.data.frame(grchim), .(seqnames,end,strand), function(x)x[x\$Chimera.start==min(x\$Chimera.start),])
       min<-ddply(as.data.frame(min), .(seqnames,start,strand), function(x)x[x\$Chimera.start==min(x\$Chimera.start),])
       max<-ddply(as.data.frame(grchim), .(seqnames,end,strand), function(x)x[x\$Chimera.end==max(x\$Chimera.end),])
       max<-ddply(as.data.frame(max), .(seqnames,start,strand), function(x)x[x\$Chimera.end==max(x\$Chimera.end),])
-      
       grfusR<-as.data.frame(grfusR)
       grfusR<-grfusR[order(grfusR[,1],grfusR[,2],grfusR[,3],grfusR[,4],decreasing=F),]
-      
       grfusR\$Chimera.start<- min\$Chimera.start
       grfusR\$Chimera.end<-max\$Chimera.end
-      
       datax<-as.data.frame(grfusR)
       colnames(datax)[1:3]<-colnames(chim)[1:3]
       colnames(datax)[5]<-colnames(chim)[4]
-      
-      grchim2 <- GRanges(seqnames = datax[,$#fastq1+11],
-                    IRanges(start = datax[,$#fastq1+12],
-                            end = datax[,$#fastq1+13]),strand=datax[,$#fastq1+14])
+      grchim2 <- GRanges(seqnames = datax[,$#fastq1+11], IRanges(start = datax[,$#fastq1+12], end = datax[,$#fastq1+13]),strand=datax[,$#fastq1+14])
       mcols(grchim2)<-datax[,-c(4,$#fastq1+11:$#fastq1+14)]
-      
       grfus<- union(grchim2,grchim2)
-      
       position<-as.data.frame(findOverlaps(grfus,grchim2))
-      
       grfus\$dup<- as.vector(table(position[,1]))
-      
       position2<-as.data.frame(findOverlaps(grfus[grfus\$dup>1],grchim2))
-      
       grchim2[ position2[,2] ]
-      
       grfus\$ID_final<-"Id"
       grfus\$ID_final[position[!duplicated(position[,1]),1]]<-grchim2\$ID[position[!duplicated(position[,1]),2]]
-      
       if(nrow(position2)!=0)
       {
         result <- aggregate(position2[,2] ~ position2[,1], data = position2, paste, collapse = "_")
         grfus\$ID_final[grfus\$dup>1]<-paste("Id",result[,2],sep="_")
       }
-      
       mcols(grfus)<-cbind(mcols(grfus), mcols(grchim2[position[!duplicated(position[,1]),2]]))
-      
       for (i in 0:$#fastq1)
       {
         mcols(grfus)[grfus\$dup>1,11+i] <- mcols(grfus)[grfus\$dup>1,11+i] +  mcols(grchim2)[position[duplicated(position[,1]),2],9+i]
       }
-      
       grfus2<-grfus
       strand(grfus2)<-"+"
       gr3<-union(grfus2,grfus2)
-      
       position3<- as.data.frame(findOverlaps(gr3,grfus2))
       gr3\$dup<-as.vector(table(position3[,1]))
-      
       position3<-as.data.frame(findOverlaps(gr3[gr3\$dup>1],grfus2))
-      
       grfus\$info [position3[,2]]<-"overlap sens opposé"
-      
       min<-ddply(as.data.frame(grchim2), .(seqnames,end,strand), function(x)x[x\$L1.start==min(x\$L1.start),])
       min<-ddply(data.frame(min), .(seqnames,start,strand), function(x)x[x\$L1.start==min(x\$L1.start),])
       max<-ddply(as.data.frame(grchim2), .(seqnames,end,strand), function(x)x[x\$L1.end==max(x\$L1.end),])
       max<-ddply(data.frame(max), .(seqnames,start,strand), function(x)x[x\$L1.end==max(x\$L1.end),])
-      
       grfus1<-as.data.frame(grfus)
       grfus1<-grfus1[order(grfus1[,1],grfus1[,2],grfus1[,3],grfus1[,4],decreasing=F),]
-      
       grfus1\$L1.start<- min\$L1.start
       grfus1\$L1.end<-max\$L1.end
-      
       dataf<-as.data.frame(grfus1)
-      
       result<-( data.frame("Chimera.Chr"= dataf\$L1.chromosome, "Chimera.Start"=apply(data.frame( dataf\$start,dataf\$end,dataf\$L1.start,dataf\$L1.end  ),1,min) , "Chimera.End"= apply(data.frame( dataf\$start,dataf\$end,dataf\$L1.start,dataf\$L1.end  ),1,max) ,"Chimera.Strand"=dataf\$L1.strand ,"L1.Chr"= dataf\$L1.chromosome, "L1.Start"=dataf\$L1.start ,"L1.End"= dataf\$L1.end , "L1.Strand"=dataf\$L1.strand , "Unique.Chr"= dataf\$seqnames, "Unique.Start"=dataf\$start , "Unique.End"= dataf\$end , "Unique.Strand"=dataf\$strand , "ID_final"=dataf\$ID_final,"info"=dataf\$info, dataf[,16:($#fastq1+16)]   )  )
-      
       result<-result[order(result[,2],decreasing=F),]
       chr<-sub("chr","",as.character(result[,1]))
       result<-result[order(as.numeric(chr)),]
       options(scipen=10)
       write.table(result,"$out2",sep="\t",row.names = F,quote = F)
-      grchim <- GRanges(seqnames = result\$L1.Chr,
-                        IRanges(start = result\$L1.Start,
-                        end = result\$L1.End),strand=result\$L1.Strand)
+      grchim <- GRanges(seqnames = result\$L1.Chr, IRanges(start = result\$L1.Start, end = result\$L1.End),strand=result\$L1.Strand)
       mcols(grchim)<-result
-    
       Rep<-read.delim("$Line_only",skip=1)
-
       Gene<-read.delim("$Hg19_refseq")
-      grLINE <- GRanges(seqnames = Rep\$genoName,
-              IRanges(start = Rep\$genoStart,
-                      end = Rep\$genoEnd),
-              repStrand=as.character(Rep\$strand),
-                                     repName =as.character(Rep\$repName))
-   
-      grGene <- GRanges(seqnames = Gene\$chrom,
-                        IRanges(start = Gene\$txStart,
-                                end = Gene\$txEnd),
-                        geneStrand=as.character(Gene\$strand),
-                                                geneName = as.character(Gene\$name2))
-      
+      grLINE <- GRanges(seqnames = Rep\$genoName, IRanges(start = Rep\$genoStart, end = Rep\$genoEnd), repStrand=as.character(Rep\$strand), repName =as.character(Rep\$repName))
+      grGene <- GRanges(seqnames = Gene\$chrom, IRanges(start = Gene\$txStart, end = Gene\$txEnd), geneStrand=as.character(Gene\$strand), geneName = as.character(Gene\$name2))
       position<-as.data.frame(findOverlaps(grchim,grLINE))
-      
       position2<-as.data.frame(findOverlaps(grchim,grGene))
-      
       grchim\$GeneName<-"no_gene"
-      
       grchim\$GeneName[position2[,1]]<- grGene\$geneName[position2[,2]]
-      
       grchim\$GeneStrand<-"*"
-      
       grchim\$GeneStrand[position2[,1]]<- grLINE\$geneStrand[position2[,2]]
-      
       grchim\$repName<-"no"
-      
       grchim\$repName[position[,1]]<- grLINE\$repName[position[,2]]
-      
       grchim\$repStart<-0
-      
       grchim\$repStart[position[,1]]<-start(grLINE[position[,2]])
-      
       grchim\$repEnd<-0
-      
       grchim\$repEnd[position[,1]]<-end(grLINE[position[,2]])
-      
       grchim\$repWidth<-0
-      
       grchim\$repWidth[position[,1]]<-width(grLINE[position[,2]])
-      
       grchim\$repStrand<-"*"
-      
       grchim\$repStrand[position[,1]]<- grLINE\$repStrand[position[,2]]
-      
       dup<-position[duplicated(position[,1]),1]
       if(length(dup != 0))
       {
@@ -1112,12 +1037,10 @@ sub save_csv{
           grchim\$repStrand[dup[i]] <-paste(grLINE\$repStrand[position[position[,1]==dup[i],2]],collapse="/")
         }
       }
-      
       final_result<-as.data.frame(grchim)
       options(scipen=10)
       write.table(final_result[,-c(1:5)],"$out3",sep="\t",row.names = F,quote = F)
-      `
-    );
+      `);
   };
   $R->stop();
   unlink @$garbage_ref;
