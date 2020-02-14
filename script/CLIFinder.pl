ddd5533f6e91923b5898484e76955c4f5aadde12
Pierre Pouchin <pierre.pouchin@uca.fr>
2020-02-14 22:23:20 +0100

    Fixed erroneous octal code

--- script/CLIFinder.pl
+++ script/CLIFinder.pl
@@ -532,8 +532,8 @@ sub get_half
     if ($line[1] == 73 || $line[1] == 89 || $line[1] == 117 || $line[1] == 69 || $line[1] == 133 || $line[1] == 181 || $line[1] == 153|| $line[1] == 137)
     {
       if ( $Bdir == 0
-              || ($Bdir == 1 && (($line[1] & 064 && $line[1] & 8) || ($line[1] & 128 && $line[1] & 4)))
-              || ($Bdir == 2 && (($line[1] & 128 && $line[1] & 8) || ($line[1] & 064 && $line[1] & 4))) )
+              || ($Bdir == 1 && (($line[1] & 64 && $line[1] & 8) || ($line[1] & 128 && $line[1] & 4)))
+              || ($Bdir == 2 && (($line[1] & 128 && $line[1] & 8) || ($line[1] & 64 && $line[1] & 4))) )
       {
         $cmp++;
         $sequence = $line[9];
